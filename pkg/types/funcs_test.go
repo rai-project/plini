fbded8d4465fb0da2e3fb21fa9a15153be099732
Abdul Dakkak <abduld@wolfram.com>
2017-04-22 16:42:37 -0500

    Add tests

--- pkg/types/funcs_test.go
+++ pkg/types/funcs_test.go
@@ -5,12 +5,25 @@ import (
 	"testing"
 
 	"github.com/chewxy/hm"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFMap(t *testing.T) {
-	fmt.Printf("%v\n", FMap)
+	assert.Equal(t, "∀[a, b, f]: a → b → f(a) → f(b)", fmt.Sprintf("%v", FMap))
+	ifmap := hm.Instantiate(NewFresh(), FMap)
+	assert.Equal(t, "a → b → c(a) → c(b)", fmt.Sprintf("%v", ifmap))
 	sub := hm.BorrowMSubs()
-	sub.Add(hm.TypeVariable('f'), hm.TypeConst("List"))
-	s := hm.Instantiate(NewFresh(), FMap).Apply(sub).(*hm.FunctionType)
-	fmt.Printf("after sub = %v\n", s)
+	sub.Add(hm.TypeVariable('x'), hm.TypeConst("List"))
+	s := ifmap.Apply(sub).(*hm.FunctionType)
+	assert.Equal(t, "a → b → c(a) → c(b)", fmt.Sprintf("%v", s))
+}
+
+func TestUnify(t *testing.T) {
+	id := hm.Instantiate(NewFresh(), Identity)
+	assert.Equal(t, "a → a", fmt.Sprintf("%v", id))
+	rhs := hm.NewFnType(Integer, Integer)
+	sub, err := hm.Unify(id, rhs)
+	assert.NoError(t, err)
+	fid := id.Apply(sub).(*hm.FunctionType)
+	assert.Equal(t, "Integer → Integer", fmt.Sprintf("%v", fid))
 }
--- pkg/types/prims.go
+++ pkg/types/prims.go
@@ -3,17 +3,17 @@ package types
 import "github.com/chewxy/hm"
 
 const (
-	Bit     hm.TypeConst = "Bit"
-	Boolean hm.TypeConst = "Boolean"
-	Int     hm.TypeConst = "Int"
-	Int8    hm.TypeConst = "Int8"
-	Int16   hm.TypeConst = "Int16"
-	Int32   hm.TypeConst = "Int32"
-	Int64   hm.TypeConst = "Int64"
-	Float   hm.TypeConst = "Float"
-	Float32 hm.TypeConst = "Float32"
-	Float64 hm.TypeConst = "Float64"
-	String  hm.TypeConst = "String"
+	Bit       hm.TypeConst = "Bit"
+	Boolean   hm.TypeConst = "Boolean"
+	Integer   hm.TypeConst = "Integer"
+	Integer8  hm.TypeConst = "Integer8"
+	Integer16 hm.TypeConst = "Integer16"
+	Integer32 hm.TypeConst = "Integer32"
+	Integer64 hm.TypeConst = "Integer64"
+	Float     hm.TypeConst = "Float"
+	Float32   hm.TypeConst = "Float32"
+	Float64   hm.TypeConst = "Float64"
+	String    hm.TypeConst = "String"
 )
 
 func NewList(t hm.Type) hm.Type {
